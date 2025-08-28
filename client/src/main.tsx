2333a9dc88a6d7a9af7139a85633e86a7e65a4e2
Vire <56460839+VireZee@users.noreply.github.com>
2025-08-28 14:28:55 +0700

    react temp

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,21 +1,18 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
 import { ApolloProvider } from '@apollo/client/react'
 import { Provider } from 'react-redux'
-import store from '@store/store'
+// import store from '@store/store'
 import App from './App'
 
-// const client = new ApolloClient({
-//     uri: `http://${import.meta.env['VITE_DOMAIN']}:${import.meta.env['VITE_SERVER_PORT']}/gql`,
-//     credentials: 'include',
-//     headers: {
-//         'Content-Type': 'application/json',
-//         'x-apollo-operation-name': 'PRNRBOpenLibraryGraphQL',
-//         'apollo-require-preflight': 'true'
-//     },
-//     cache: new InMemoryCache({ addTypename: false })
-// })
+const client = new ApolloClient({
+    link: new HttpLink({
+        uri: `http://${import.meta.env['VITE_DOMAIN']}:${import.meta.env['VITE_SERVER_PORT']}/gql`,
+        credentials: 'include'
+    }),
+    cache: new InMemoryCache()
+})
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <ApolloProvider client={client}>
