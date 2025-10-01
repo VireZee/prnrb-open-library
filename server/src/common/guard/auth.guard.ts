cd66560f361ada9c58068ed14ed763bebf5b3e20
Vire <56460839+VireZee@users.noreply.github.com>
2025-10-01 08:45:47 +0700

    guard temp

--- server/src/common/guard/auth.guard.ts
+++ server/src/common/guard/auth.guard.ts
@@ -3,10 +3,10 @@ import { GqlExecutionContext } from '@nestjs/graphql'
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+    constructor() {}
     async canActivate(context: ExecutionContext): Promise<boolean> {
         const ctx = GqlExecutionContext.create(context)
         const req = ctx.getContext().req
         const token = req.token
-        return false
     }
 }
