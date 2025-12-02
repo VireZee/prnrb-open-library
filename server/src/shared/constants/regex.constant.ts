68c9df0fdef614083b339970bca3fae6b6a9b9d3
Vire <56460839+VireZee@users.noreply.github.com>
2025-12-02 13:08:29 +0700

    regex constants

---
+++ server/src/shared/constants/regex.constant.ts
@@ -0,0 +1,3 @@
+export const USERNAME_REGEX = /^[\w\d]+$/
+export const EMAIL_REGEX = /^[\w.-]+@([\w-]+\.)+[A-Za-z]{2,}$/
+export const SANITIZE_REGEX = /[^a-zA-Z0-9]/g
--- server/src/shared/utils/security/services/sanitize.service.ts
+++ server/src/shared/utils/security/services/sanitize.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common'
+import { SANITIZE_REGEX } from '@shared/constants/regex.constant.js'
 
 @Injectable()
 export class SanitizeService {
-    sanitize(id: string): string { return `${id.replace(/[^a-zA-Z0-9]/g, '')}` }
+    sanitize(id: string): string { return `${id.replace(SANITIZE_REGEX, '')}` }
     sanitizeRedisKey(name: string, key: string): string { return `${name}:${this.sanitize(key)}` }
 }
