757585e2be09540aa6907b5fbc07d46cec433aa8
Vire <56460839+VireZee@users.noreply.github.com>
2025-12-03 15:59:59 +0700

    retry temp

--- server/src/common/utils/retry.util.ts
+++ server/src/common/utils/retry.util.ts
@@ -1,12 +1,11 @@
 import { setTimeout } from 'node:timers/promises'
-
-export async function retry<T>(fn: () => Promise<T>, opts: {
+export default async <T>(fn: () => Promise<T>, opts: {
     retries?: number
     baseDelay?: number
     backoff?: number
     jitter?: boolean
     retryOn?: (err: any) => boolean
-} = {}): Promise<T> {
+} = {}): Promise<T> => {
     const { retries = 3, baseDelay = 200, backoff = 2, jitter = true, retryOn = () => true } = opts
     let lastErr
     let delay = baseDelay
