e0afd2ab2b109cf841f7d6f47456776f7d30428b
Vire <56460839+VireZee@users.noreply.github.com>
2025-12-03 14:37:52 +0700

    pub sub temp

--- server/src/infrastructure/cache/services/redis.service.ts
+++ server/src/infrastructure/cache/services/redis.service.ts
@@ -5,11 +5,19 @@ import { createClient, type RedisClientType } from 'redis'
 @Injectable()
 export class RedisService implements OnModuleInit, OnModuleDestroy {
     redis: RedisClientType
+    pub: RedisClientType
+    sub: RedisClientType
     async onModuleInit(): Promise<void> {
         this.redis = createClient({ url: process.env['REDIS_URL']! })
+        this.pub = this.redis.duplicate()
+        this.sub = this.redis.duplicate()
         await this.redis.connect()
+        await this.pub.connect()
+        await this.sub.connect()
     }
     async onModuleDestroy(): Promise<void> {
         await this.redis.close()
+        await this.pub.close()
+        await this.sub.close()
     }
 }
