ad5aec980d061b4b6042a856990b08fce8016c43
Vire <56460839+VireZee@users.noreply.github.com>
2025-12-08 09:49:59 +0700

    .

--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -1,18 +1,14 @@
 import { Module } from '@nestjs/common'
-import { SecurityModule } from '@shared/utils/security/security.module.js'
-import { FormatterModule } from '@shared/utils/formatter/formatter.module.js'
-import { MiscModule } from '@shared/utils/misc/misc.module.js'
-import { AccountModule } from '@shared/account/account.module.js'
+import { UtilModule } from '@shared/utils/util.module.js'
 import { RegisterPipe } from '@common/pipes/auth/register.pipe.js'
 import { AuthResolver } from './auth.resolver.js'
 import { RegisterService } from './services/register.service.js'
 import { VerifyService } from './services/verify.service.js'
 import { ResendService } from './services/resend.service.js'
 import { LoginService } from './services/login.service.js'
-import { ValidationModule } from '@shared/utils/validation/validation.module.js'
 
 @Module({
-    imports: [SecurityModule, ValidationModule, FormatterModule, MiscModule, AccountModule],
+    imports: [UtilModule],
     providers: [RegisterPipe, AuthResolver, RegisterService, VerifyService, ResendService, LoginService]
 })
 export class AuthModule {}
--- server/src/modules/auth/services/verification.service.ts
+++ server/src/modules/auth/services/verification.service.ts
@@ -1,19 +1,16 @@
-import { Injectable, Res } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { RedisService } from '@infrastructure/cache/services/redis.service.js'
 import { SecurityService } from '@shared/utils/services/security.service.js'
 import { EmailService } from '@infrastructure/email/email.service.js'
-import { PrismaService } from '@infrastructure/database/prisma.service.js'
-import { FormatterService } from '@shared/utils/services/formatter.service.js'
 
 @Injectable()
-export class AccountService {
+export class VerificationService {
     constructor(
-        private readonly prismaService: PrismaService,
+
         private readonly redisService: RedisService,
         private readonly securityService: SecurityService,
-        private readonly emailService: EmailService,
-        private readonly formatService: FormatterService
-    ) { }
+        private readonly emailService: EmailService
+    ) {}
     async generateCode(keyName: string, user: { id: string, email: string }, isForget: boolean): Promise<void> {
         const key = this.securityService.sanitizeRedisKey(keyName, user.id)
         const randomString = nodeCrypto.randomBytes(64).toString('hex')
@@ -24,7 +21,6 @@ export class AccountService {
         return await this.emailService.verifyEmail(user.email, verificationCode, user.id)
     }
     cookie(user: { id: string }, @Res({ passthrough: true }) res: Res): void {
-        const token = this.securityService.jwtSign(user.id)
         res.cookie('!', token, {
             maxAge: 1000 * 60 * 60 * 24 * 30,
             httpOnly: true,
