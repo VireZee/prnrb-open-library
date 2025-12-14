09bce41aa716f729d6211c6b4d6b9629337ba540
Zee <56460839+VireZee@users.noreply.github.com>
2025-12-14 21:36:45 +0700

    auth controller

--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+import { AuthController } from '@common/controllers/auth.controller.js'
 import { UtilModule } from '@shared/utils/util.module.js'
 import { RegisterPipe } from '@common/pipes/auth/register.pipe.js'
 import { AuthResolver } from './auth.resolver.js'
@@ -8,9 +9,11 @@ import { RegisterService } from './services/register.service.js'
 import { VerifyService } from './services/verify.service.js'
 import { ResendService } from './services/resend.service.js'
 import { LoginService } from './services/login.service.js'
+import { SettingService } from './services/settings.service.js'
 
 @Module({
+    controllers: [AuthController],
     imports: [UtilModule],
-    providers: [RegisterPipe, AuthResolver, VerificationService, RateLimiterService, RegisterService, VerifyService, ResendService, LoginService]
+    providers: [RegisterPipe, AuthResolver, VerificationService, RateLimiterService, RegisterService, VerifyService, ResendService, LoginService, SettingService]
 })
 export class AuthModule {}
