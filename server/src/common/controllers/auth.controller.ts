efef6d45def586342e81f181d530c5399f9c8b1d
Zee <56460839+VireZee@users.noreply.github.com>
2025-12-14 21:34:39 +0700

    auth controller

--- server/src/common/controllers/auth.controller.ts
+++ server/src/common/controllers/auth.controller.ts
@@ -1,7 +1,62 @@
-import { Controller, Post } from '@nestjs/common'
+import { Controller, Post, Req, Res } from '@nestjs/common'
+import { RedisService } from '@infrastructure/cache/services/redis.service.js'
+import { SecurityService } from '@shared/utils/services/security.service.js'
+import ERROR from '../constants/error.constant.js'
 
 @Controller('auth')
 export class AuthController {
+    constructor(
+        private readonly redisService: RedisService,
+        private readonly securityService: SecurityService
+    ) {}
     @Post()
-    async auth() {}
+    async auth(@Req() req: Req, @Res({ passthrough: true }) res: Res) {
+        const rt = req.cookies['!']
+        if (!rt) throw { code: ERROR.UNAUTHENTICATED }
+        const refreshKey = this.securityService.sanitizeRedisKey('refresh', rt)
+        const ua = req.headers['user-agent'] ?? ''
+        const lang = req.headers['accept-language'] ?? ''
+        const encoding = req.headers['accept-encoding'] ?? ''
+        const secChUa = req.headers['sec-ch-ua'] ?? ''
+        const secChUaPlatform = req.headers['sec-ch-ua-platform'] ?? ''
+        const { platform = '', tz = '', screenRes = '', colorDepth = '', devicePixelRatio = '', touchSupport = '', hardwareConcurrency = '' } = req.body.identity
+        const fingerprint = nodeCrypto.createHash('sha256').update(
+            ua +
+            lang +
+            encoding +
+            secChUa +
+            secChUaPlatform +
+            platform +
+            tz +
+            screenRes +
+            colorDepth +
+            devicePixelRatio +
+            touchSupport +
+            hardwareConcurrency
+        ).digest('hex')
+        const session = await this.redisService.redis.HGETALL(refreshKey)
+        if (fingerprint !== session['fingerprint']) throw { code: ERROR.UNAUTHENTICATED }
+        const newRt = nodeCrypto.randomBytes(32).toString('base64url')
+        const newRefreshKey = `refresh:${newRt}`
+        await this.redisService.redis.HSET(newRefreshKey, {
+            id: session['id']!,
+            fingerprint
+        })
+        const ttl = await this.redisService.redis.TTL(refreshKey)
+        if (ttl <= 0) throw { code: ERROR.UNAUTHENTICATED }
+        await this.redisService.redis.EXPIRE(newRefreshKey, ttl)
+        await this.redisService.redis.DEL(refreshKey)
+        const at = nodeCrypto.randomBytes(32).toString('base64url')
+        const accessKey = `access:${at}`
+        await this.redisService.redis.SET(accessKey, session['id']!, { expiration: { type: 'EX', value: 60 * 5 } })
+        res.cookie('!', newRt, {
+            path: '/auth',
+            maxAge: 1000 * ttl,
+            httpOnly: true,
+            secure: true,
+            sameSite: 'lax',
+            priority: 'high'
+        })
+        return at
+    }
 }
--- server/src/modules/auth/strategies/opaque.strategy.ts
+++ server/src/modules/auth/strategies/opaque.strategy.ts
@@ -14,16 +14,6 @@ export class OpaqueStrategy extends PassportStrategy(Strategy) {
         private readonly securityService: SecurityService,
         private readonly formatterService: FormatterService
     ) { super() }
-    // private async user(id: string): Promise<unknown> {
-    //     const key = this.securityService.sanitizeRedisKey('user', id)
-    //     const cache = await this.redisService.redis.json.GET(key)
-    //     if (cache) return cache
-    //     const user = await this.prismaService.user.findUnique({ where: { id } })
-    //     if (!user) return null
-    //     await this.redisService.redis.json.SET(key, '$', this.formatterService.formatUser(user))
-    //     await this.redisService.redis.EXPIRE(key, 86400)
-    //     return this.formatterService.formatUser(user)
-    // }
     async validate(req: Req): Promise<unknown> {
         const at = req.headers.authorization
         if (!at) return null
@@ -40,27 +30,5 @@ export class OpaqueStrategy extends PassportStrategy(Strategy) {
         await this.redisService.redis.json.SET(userKey, '$', this.formatterService.formatUser(user))
         await this.redisService.redis.EXPIRE(userKey, 60 * 60 * 24 * 30)
         return this.formatterService.formatUser(user)
-        // const ua = req.headers['user-agent'] ?? ''
-        // const lang = req.headers['accept-language'] ?? ''
-        // const encoding = req.headers['accept-encoding'] ?? ''
-        // const secChUa = req.headers['sec-ch-ua'] ?? ''
-        // const secChUaPlatform = req.headers['sec-ch-ua-platform'] ?? ''
-        // const { platform = '', tz = '', screenRes = '', colorDepth = '', devicePixelRatio = '', touchSupport = '', hardwareConcurrency = '' } = req.body.identity
-        // const fingerprint = nodeCrypto.createHash('sha256').update(
-        //     ua +
-        //     lang +
-        //     encoding +
-        //     secChUa +
-        //     secChUaPlatform +
-        //     platform +
-        //     tz +
-        //     screenRes +
-        //     colorDepth +
-        //     devicePixelRatio +
-        //     touchSupport +
-        //     hardwareConcurrency
-        // ).digest('hex')
-        // if (fingerprint !== session['fingerprint']) return null
-        // return this.user(session['id']!)
     }
 }
