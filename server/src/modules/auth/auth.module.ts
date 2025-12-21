6cb0dcab4b773fa7b822685590e95ac5f9c63041
Zee <56460839+VireZee@users.noreply.github.com>
2025-12-21 22:18:16 +0700

    module temp

--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common'
 import { AuthController } from '@common/controllers/auth.controller.js'
+import { EmailModule } from '@infrastructure/email/email.module.js'
 import { UtilModule } from '@shared/utils/util.module.js'
 import { RegisterInterceptor } from '@common/interceptors/auth/register.interceptor.js'
-import { LoginInterceptor } from '@common/interceptors/auth/login.interceptor.js'
-import { SettingsInterceptor } from '@common/interceptors/auth/settings.interceptor.js'
 import { RegisterPipe } from '@common/pipes/auth/register.pipe.js'
+import { LoginInterceptor } from '@common/interceptors/auth/login.interceptor.js'
 import { LoginPipe } from '@common/pipes/auth/login.pipe.js'
+import { SettingsInterceptor } from '@common/interceptors/auth/settings.interceptor.js'
 import { SettingsPipe } from '@common/pipes/auth/settings.pipe.js'
+import { LogoutInterceptor } from '@common/interceptors/auth/logout.interceptor.js'
 import { AuthResolver } from './auth.resolver.js'
 import { VerificationService } from './services/verification.service.js'
 import { RateLimiterService } from './services/rate-limiter.service.js'
@@ -15,17 +17,19 @@ import { VerifyService } from './services/verify.service.js'
 import { ResendService } from './services/resend.service.js'
 import { LoginService } from './services/login.service.js'
 import { SettingService } from './services/settings.service.js'
+import { LogoutService } from './services/logout.service.js'
 
 @Module({
     controllers: [AuthController],
-    imports: [UtilModule],
+    imports: [EmailModule, UtilModule],
     providers: [
         RegisterInterceptor,
-        LoginInterceptor,
-        SettingsInterceptor,
         RegisterPipe,
+        LoginInterceptor,
         LoginPipe,
+        SettingsInterceptor,
         SettingsPipe,
+        LogoutInterceptor,
         AuthResolver,
         VerificationService,
         RateLimiterService,
@@ -33,7 +37,8 @@ import { SettingService } from './services/settings.service.js'
         VerifyService,
         ResendService,
         LoginService,
-        SettingService
+        SettingService,
+        LogoutService
     ]
 })
 export class AuthModule {}
