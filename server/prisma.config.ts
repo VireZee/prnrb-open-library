5dea8ff088f630979f8564a3fdfab5d0d078ada4
9223372036854775807 18446744073709551615 <56460839+virezee@users.noreply.github.com>
2025-12-22 22:01:28 +0700

    Update prisma.config.ts

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, env } from 'prisma/config'
 export default defineConfig({
     schema: 'src/infrastructure/prisma/schema.prisma',
     migrations: {
-        path: 'src/infrastructure/prisma/migrations',
+        path: 'src/infrastructure/prisma/migrations'
     },
     datasource: {
         url: env("DB_URL")
