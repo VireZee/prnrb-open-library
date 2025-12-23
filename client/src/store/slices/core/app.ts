553dd51c5f340d355d2d01c06c7f3ea8f0839c13
Vire <56460839+VireZee@users.noreply.github.com>
2025-12-23 11:48:16 +0700

    update react

--- client/src/store/slices/core/app.ts
+++ client/src/store/slices/core/app.ts
@@ -4,14 +4,18 @@ import type { GlobalUserState } from '@type/redux/state'
 import type User from '@type/redux/auth/user'
 
 const initialState: GlobalUserState = {
-    search: '',
+    accessToken: null,
     user: undefined,
-    verified: null
+    verified: null,
+    search: ''
 }
 const app = createSlice({
     name: 'app',
     initialState,
     reducers: {
+        setAccessToken: (state, { payload }: PayloadAction<string | null>) => {
+            state['accessToken'] = payload
+        },
         setUser: (state, { payload }: PayloadAction<null | User>) => {
             state['user'] = payload
         },
@@ -23,5 +27,5 @@ const app = createSlice({
         }
     }
 })
-export const { setUser, setVerified, setSearch } = app.actions
+export const { setAccessToken, setUser, setVerified, setSearch } = app.actions
 export default app.reducer
