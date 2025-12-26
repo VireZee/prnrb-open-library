8b3ca090fb0bf6f4e8cc5433e79fa0db371c2ca7
Zee <56460839+VireZee@users.noreply.github.com>
2025-12-26 15:12:18 +0700

    main tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -62,16 +62,13 @@ const errorLink = new ErrorLink(({ error, operation, forward }) => {
             } catch {
                 store.dispatch(setAccessToken(null))
                 store.dispatch(setUser(null))
-                return
+                observer.complete()
             }
         })()
     })
 })
 const client = new ApolloClient({
-    link: new HttpLink({
-        uri: `http://${import.meta.env['VITE_DOMAIN']}:${import.meta.env['VITE_SERVER_PORT']}/gql`,
-        credentials: 'include'
-    }),
+    link: ApolloLink.from([errorLink, authLink, httpLink]),
     cache: new InMemoryCache()
 })
 createRoot(document.getElementById('root')!).render(
