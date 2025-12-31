5fa277ce812e342dcebaca4de2b9751775dc16b3
Zee <56460839+VireZee@users.noreply.github.com>
2025-12-31 15:30:00 +0700

    .

--- server/src/infrastructure/cache/cache.module.ts
+++ server/src/infrastructure/cache/cache.module.ts
@@ -1,12 +1,14 @@
 import { Global, Module } from '@nestjs/common'
 import { UtilModule } from '@shared/utils/util.module.js'
 import { RedisService } from './services/redis.service.js'
+import { PublisherService } from './services/publisher.service.js'
+import { SubscriberService } from './services/subscriber.service.js'
 import { CacheService } from './services/cache.service.js'
 
 @Global()
 @Module({
     imports: [UtilModule],
-    providers: [RedisService, CacheService],
-    exports: [RedisService, CacheService]
+    providers: [RedisService, PublisherService, SubscriberService, CacheService],
+    exports: [RedisService, PublisherService, SubscriberService, CacheService]
 })
 export class CacheModule {}
