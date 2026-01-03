e6bdd168151f072523539b676f5c0a4fcca28c31
Zee <56460839+virezee@users.noreply.github.com>
2026-01-03 23:18:23 +0700

    google temp

--- server/src/common/controllers/google.controller.ts
+++ server/src/common/controllers/google.controller.ts
@@ -1,15 +1,35 @@
-import { Controller, Get, UseGuards } from '@nestjs/common'
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common'
 import { GoogleAuthGuard } from '@common/guards/google.guard.js'
 
 @Controller('auth/google')
 export class GoogleController {
     @Get('register')
     @UseGuards(GoogleAuthGuard)
-    _(): void {}
+    _(): void { }
     @Get('login')
     @UseGuards(GoogleAuthGuard)
-    __(): void {}
+    __(): void { }
     @Get('connect')
     @UseGuards(GoogleAuthGuard)
-    ___(): void {}
+    ___(): void { }
+    @Get('callback')
+    async ____(@Req() req: Req, @Res() res: Res) {
+        const user = req.user
+        if (!user) {
+            return res.send(`
+                <script>
+                    window.opener.postMessage({ message: '${info.message}' }, 'http://${process.env['DOMAIN']}:${process.env['CLIENT_PORT']}')
+                    window.close()
+                </script>
+            `)
+        }
+        if (err) {
+            return res.send(`
+                <script>
+                    window.opener.postMessage({ message: '${err}' }, 'http://${process.env['DOMAIN']}:${process.env['CLIENT_PORT']}')
+                    window.close()
+                </script>
+            `)
+        }
+    }
 }
